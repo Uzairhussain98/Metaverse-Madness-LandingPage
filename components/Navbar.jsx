5f02118739b869c52a071ee36da2c2fe4b17a3a9
Uzair Hussain <uzairhussain98@hotmail.com>
2022-12-20 23:46:53 +0500

    completed nav bar and hero

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,32 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    className={`${styles.xPaddings} py-8 relative`}
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="search"
+        className="h-[24px] w-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        METAVERSUS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className=" h-[24px] w-[24px] object-contain"
+      />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
